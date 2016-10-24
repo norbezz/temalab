ec084bc22ecae8fdcb441c31ef31d709917b2151
norbezz <norbezz@gmail.com>
2016-10-24 21:50:52 +0200

    Working code

--- aramkor/aramkor.ino
+++ aramkor/aramkor.ino
@@ -5,6 +5,8 @@ Servo myservo;
 int pos = 0;
 int left = LOW;
 int right = LOW;
+int led = 4;
+int onboard_led = 2;
 int leftbutton = 12;
 int rightbutton = 13;
 int servomotor = 15;
@@ -13,24 +15,38 @@ void setup() {
   pinMode(rightbutton, INPUT);
   pinMode(leftbutton, INPUT);
   pinMode(servomotor, OUTPUT);
+  pinMode(onboard_led, OUTPUT);
+  pinMode(led, OUTPUT);
+  digitalWrite(onboard_led,HIGH);
   myservo.attach(servomotor);
+  myservo.write(90);
 }
-void loop() {
-  left = digitalRead(leftbutton);
-  if (left == HIGH){
-    for (pos = 0; pos <= 180; pos += 1) {
-      // in steps of 1 degree
+void loop() {left = digitalRead(leftbutton);
+  if (left == HIGH) {
+    digitalWrite(led,HIGH);
+    for (pos = 90; pos <= 180; pos += 1) {
       myservo.write(pos);
       delay(15);
     }
+    for (pos = 180; pos >= 90; pos -= 1) {
+      myservo.write(pos);
+      delay(15);
+    }
+    digitalWrite(led,LOW);
   }
-  left = LOW;
+  myservo.write(90);
   right = digitalRead(rightbutton);
-  if(right == HIGH){
-    for (pos = 180; pos >= 0; pos -= 1) {
+  if (right == HIGH) {
+    digitalWrite(onboard_led,LOW);
+    for (pos = 90; pos >= 0; pos -= 1) {
       myservo.write(pos);
       delay(15);
     }
+    for (pos = 0; pos <= 90; pos += 1) {
+      myservo.write(pos);
+      delay(15);
+    }
+    digitalWrite(onboard_led,HIGH);
   }
-  right = LOW;
+  myservo.write(90);
 }
